e0affc2801eb8148cc3c9694d940a159b2d428ea
MendelVV <mandel.vasilij@yandex.ru>
2019-03-29 11:34:56 +1000

    Replaced tab "Voted" by tab "My Activities"

--- app/src/main/java/ru/mendel/apps/rcoko27/activities/MainActivity.kt
+++ app/src/main/java/ru/mendel/apps/rcoko27/activities/MainActivity.kt
@@ -2,16 +2,69 @@ package ru.mendel.apps.rcoko27.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.mendel.apps.rcoko27.R
+import ru.mendel.apps.rcoko27.fragments.ActivitiesFragment
 import ru.mendel.apps.rcoko27.fragments.EventsListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mItemEvents:MenuItem
+    private lateinit var mItemActivities:MenuItem
+    private lateinit var mCurrentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment = EventsListFragment()
-        supportFragmentManager.beginTransaction().add(R.id.fragment_layout, fragment).commit()
+        setBottomMenu()
+        mCurrentFragment = EventsListFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_layout, mCurrentFragment).commit()
+    }
+
+    private fun setBottomMenu(){
+        val menu = bottom_menu.menu
+        mItemEvents = menu.findItem(R.id.action_events)
+        mItemEvents.setOnMenuItemClickListener { setEventsFragment() }
+
+        mItemActivities = menu.findItem(R.id.action_activities)
+        mItemActivities.setOnMenuItemClickListener { setActivitiesFragment() }
+    }
+
+    private fun setEventsFragment(): Boolean {
+        Log.d("MyTag","setEvents")
+        if (mCurrentFragment is ActivitiesFragment){
+            mItemActivities.isChecked = false
+            mItemEvents.isChecked = true
+            mCurrentFragment = EventsListFragment()
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.in_left,R.anim.out_right)
+                .replace(R.id.fragment_layout, mCurrentFragment)
+                .commit()
+            return true
+        }else{
+
+            return false
+        }
+    }
+
+    private fun setActivitiesFragment(): Boolean {
+        Log.d("MyTag","setActivities")
+        if (mCurrentFragment is EventsListFragment){
+            mItemEvents.isChecked = false
+            mItemActivities.isChecked = true
+            mCurrentFragment = ActivitiesFragment()
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.in_right,R.anim.out_left)
+                .replace(R.id.fragment_layout, mCurrentFragment)
+                .commit()
+            return true
+        }else{
+            return false
+        }
+
     }
 }
---
+++ app/src/main/java/ru/mendel/apps/rcoko27/fragments/ActivitiesFragment.kt
@@ -0,0 +1,18 @@
+package ru.mendel.apps.rcoko27.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.mendel.apps.rcoko27.R
+
+class ActivitiesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.activities_fragment,container,false)
+
+
+        return view
+    }
+}
